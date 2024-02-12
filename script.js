c23315acfe9ceff512a5cfdaa447509f5c42c99a
Chris Intriago <35647407+chintriago@users.noreply.github.com>
2024-02-12 08:35:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,19 @@ if (localStorage.getItem('counter')) {
 // function to play audio
 function playSound(name) {
     audio = new Audio('sounds/' + name + '.mp3');
+    audio.volume = 0.1;
     audio.play();
 }
 
 // When plus button is clicked counter is added by 1.
+// If statement added to keep track of counter after page refresh.
 // Counter value stored in localStorage.
 // Plus audio is played.
 // Then the number html is updated to that amount.
 buttonPlus.addEventListener('click', function () {
+    if (localStorage.getItem('counter')) {
+        counter = parseInt(localStorage.getItem('counter'));
+    }
     counter++;
     localStorage.setItem('counter', counter);
     playSound('plus');
@@ -28,23 +33,28 @@ buttonPlus.addEventListener('click', function () {
 });
 
 // When minus button is clicked counter is subtracted by 1.
+// If statement added to keep track of counter after page refresh.
 // Counter value stored in localStorage.
 // Minus audio is played.
 // Then the number html is updated to that amount.
 buttonMinus.addEventListener('click', function () {
+    if (localStorage.getItem('counter')) {
+        counter = parseInt(localStorage.getItem('counter'));
+    }
     counter--;
-    localStorage.setItem('counter', counter);
     playSound('minus');
     number.innerHTML = counter.toString();
+    localStorage.setItem('counter', counter);
 });
 
 // When reset button is clicked counter is equal to 0.
+// If statement added to keep track of counter after page refresh.
 // Counter value stored in localStorage.
 // Reset audio is played.
 // Then the number html is updated to that amount.
 buttonReset.addEventListener('click', function () {
     counter = 0;
-    localStorage.setItem('counter', counter);
     playSound('reset');
     number.innerHTML = counter.toString();
+    localStorage.setItem('counter', counter);
 });
